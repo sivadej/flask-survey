aa8a868ba072b8be75589450d0a42b33fe026c4a
Sivadej K <bombyk@gmail.com>
2020-02-25 16:32:09 -0900

    needs refactoring - see todo

--- app.py
+++ app.py
@@ -23,16 +23,17 @@ toolbar = DebugToolbarExtension(app)
 responses = []
 survey = satisfaction_survey
 
+
 @app.route('/')
 def index():
     return render_template('survey_index.html', title=survey.title, instructions=survey.instructions)
 
-@app.route('/questions/<int:id>', methods=['GET','POST'])
+
+@app.route('/questions/<int:id>', methods=['GET', 'POST'])
 def questions(id):
     # protect questions - keep track of progress and dont allow user to manually enter ID as url,
     #   return them to current question
-    
-    
+
     if request.method == 'POST':
         user_response = request.form['response']
         print(f'user_response received for question {id}: {user_response}')
@@ -42,15 +43,25 @@ def questions(id):
             print(response)
         # redirect to thank you page once final question is answered
         if next_id >= len(survey.questions):
-            return redirect(url_for('results'))    
+            return redirect(url_for('results'))
         else:
             return redirect(url_for('questions', id=next_id))
     return render_template('survey_question.html', title=survey.title, question=survey.questions[id])
+
 
 @app.route('/results')
 def results():
     return render_template('survey_results.html', title=survey.title, results=responses)
 
+
 @app.route('/thanks')
 def thanks():
     return "thank you!"
+
+
+# 2/25 TO-DO refactor
+# split questions route into questions and answers
+# questions should use GET that renders the question HTML
+# which has a form that contains a POST method to /answer route.
+# logic that checks survey progress should be handled in /questions
+# logic that directs completed user survey should be handled in /answer
